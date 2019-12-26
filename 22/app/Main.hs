0736aaaba12bd7e96ba10e1f5913bfa0eff3c481
Matthew Thornton <m.thornton@winton.com>
2019-12-26 01:08:17 +0000

    It's a mess but it finally works.

--- 22/app/Main.hs
+++ 22/app/Main.hs
@@ -2,16 +2,43 @@ module Main where
 
 import           Lib
 import Data.List
+import Data.Maybe
 
 main :: IO ()
 main = do
     f <- readFile "input.txt"
     let shuffles = parseInstructions $ lines f
-    -- print $ shuffled shuffles [0..9]
-    print $ answer1 $ lines f
+    let deckSize = 119315717514047
+    let position = 2020
 
-shuffled :: [Shuffle] -> [Int] -> [Int]
-shuffled shuffles deck = map fst 
-    $ sortOn snd 
-    $ zip deck 
-    $ map (\i -> shuffle (length deck) i shuffles) [0.. (length deck - 1)]
+    let series = take 3 $ manyShuffles deckSize position shuffles
+    print series
+
+    let coeffs = combineShuffles deckSize shuffles
+    print coeffs
+
+    print "Trying first in series"
+    print $ applyCoeffs deckSize (head series) coeffs
+    print $ applyCoeffs deckSize (series !! 1) coeffs
+    print "Should be the same as above"
+    print $ applyCoeffs deckSize (head series) (combineCoeffs deckSize coeffs coeffs)
+
+    putStrLn ""
+    print "Inverting..."
+    let invSeries = take 3 $ manyInvShuffles deckSize position shuffles
+    print invSeries
+    
+    let mmi = fromMaybe (error "Boom") (modInv (a coeffs) deckSize)
+    let invCoeffs = Coeffs { a = mmi `mod` deckSize, b = (-b coeffs * mmi) `mod` deckSize}
+    print invCoeffs
+    
+    print "Testing inverse"
+    print $ applyCoeffs deckSize (head invSeries) invCoeffs
+    print $ applyCoeffs deckSize (invSeries !! 1) invCoeffs
+    print "Should be the same as above"
+    print $ applyCoeffs deckSize (head invSeries) (combineCoeffs deckSize invCoeffs invCoeffs)
+
+    print "Answer"
+    let finalCoeffs = rapidCombine 101741582076661 deckSize invCoeffs
+    print finalCoeffs
+    print $ applyCoeffs deckSize position finalCoeffs
--- 22/src/Lib.hs
+++ 22/src/Lib.hs
@@ -7,16 +7,59 @@ import           Data.Char
 import           Data.List
 import           Data.Maybe
 
-data Shuffle = Cut Int | Reverse | DealIncrement Int
+data Shuffle = Cut Integer | Reverse | DealIncrement Integer
     deriving (Eq, Ord, Show)
-type DeckSize = Int
-type Position = Int
+data Coeffs = Coeffs { a :: Integer, b :: Integer }
+    deriving (Eq, Ord, Show)
+type DeckSize = Integer
+type Position = Integer
 
 answer1 :: [String] -> Position
 answer1 input = shuffle 10007 2019 $ parseInstructions input
 
-answer2 :: [String] -> Int
-answer2 input = 0
+answer2 :: [String] -> Coeffs
+answer2 input = 
+    let shuffles = parseInstructions input
+        invShuffles = reverse shuffles
+    in invShuffleCoeffs 119315717514047 2020 invShuffles
+
+combineShuffles :: DeckSize -> [Shuffle] -> Coeffs
+combineShuffles deckSize = foldl (\coeffs shuff -> case shuff of
+    Reverse -> coeffs { a = (-a coeffs) `mod` deckSize, b = (-b coeffs - 1) `mod` deckSize}
+    Cut n -> coeffs { b = (b coeffs - n) `mod` deckSize }
+    DealIncrement n -> coeffs { a = (a coeffs * n) `mod` deckSize, b = (b coeffs * n) `mod` deckSize }) Coeffs { a = 1, b = 0 }
+
+applyCoeffs :: DeckSize -> Integer -> Coeffs -> Integer
+applyCoeffs deckSize x coeffs = (((a coeffs * x) `mod` deckSize) + b coeffs) `mod` deckSize
+
+rapidCombine :: Integer -> DeckSize -> Coeffs -> Coeffs
+rapidCombine 1 deckSize coeffs = coeffs 
+rapidCombine n deckSize coeffs
+    | n `mod` 2 == 0 = 
+        let halfCoeffs = rapidCombine (n `div` 2) deckSize coeffs 
+        in combineCoeffs deckSize halfCoeffs halfCoeffs
+    | otherwise = combineCoeffs deckSize coeffs (rapidCombine (n - 1) deckSize coeffs)
+
+combineCoeffs :: DeckSize -> Coeffs -> Coeffs -> Coeffs
+combineCoeffs deckSize first second = Coeffs { 
+    a = (a second * a first) `mod` deckSize,
+    b = ((a second * b first) + b second) `mod` deckSize }
+
+manyInvShuffles :: DeckSize -> Position -> [Shuffle] -> [Integer]
+manyInvShuffles deckSize pos shuffles = 
+    unfoldr (\seed -> Just (seed, invShuffle deckSize seed $ reverse shuffles)) pos
+
+manyShuffles :: DeckSize -> Position -> [Shuffle] -> [Integer]
+manyShuffles deckSize pos shuffles = 
+    unfoldr (\seed -> Just (seed, shuffle deckSize seed shuffles)) pos
+
+invShuffleCoeffs :: DeckSize -> Position -> [Shuffle] -> Coeffs
+invShuffleCoeffs deckSize pos shuffles =
+    let firstUnshuffle = invShuffle deckSize pos shuffles
+    in Coeffs { a = firstUnshuffle `div` pos, b = firstUnshuffle `mod` pos }
+
+invShuffle :: DeckSize -> Position -> [Shuffle] -> Position
+invShuffle deckSize = foldl (doInvShuffle deckSize)
 
 shuffle :: DeckSize -> Position -> [Shuffle] -> Position
 shuffle deckSize = foldl (doShuffle deckSize)
@@ -27,6 +70,12 @@ doShuffle deckSize position shuffle = case shuffle of
     Cut n -> (position - n) `mod` deckSize
     DealIncrement n -> (position * n) `mod` deckSize
 
+doInvShuffle :: DeckSize -> Position -> Shuffle -> Position
+doInvShuffle deckSize position shuffle = case shuffle of
+    Reverse -> (deckSize - 1) - position
+    Cut n -> (position + n) `mod` deckSize
+    DealIncrement n -> (position * fromMaybe (error "No modInv") (modInv n deckSize)) `mod` deckSize
+
 parseInstructions :: [String] -> [Shuffle]
 parseInstructions = map parseInstruction
 
@@ -35,3 +84,26 @@ parseInstruction i = case words i of
     ["deal", "into", "new", "stack"] -> Reverse
     ["cut", x]                       -> Cut $ read x
     ["deal", "with", "increment", x] -> DealIncrement $ read x
+
+-- Given a and m, return Just x such that ax = 1 mod m.
+-- If there is no such x return Nothing.
+modInv :: Integer -> Integer -> Maybe Integer
+modInv a m
+  | 1 == g = Just (mkPos i)
+  | otherwise = Nothing
+  where
+    (i, _, g) = gcdExt a m
+    mkPos x
+      | x < 0 = x + m
+      | otherwise = x
+ 
+-- Extended Euclidean algorithm.
+-- Given non-negative a and b, return x, y and g
+-- such that ax + by = g, where g = gcd(a,b).
+-- Note that x or y may be negative.
+gcdExt :: Integer -> Integer -> (Integer, Integer, Integer)
+gcdExt a 0 = (1, 0, a)
+gcdExt a b =
+  let (q, r) = a `quotRem` b
+      (s, t, g) = gcdExt b r
+  in (t, s - q * t, g)
